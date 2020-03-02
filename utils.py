4d3f31dbd11c498741c79472f9973354992d9c8d
Aisha Alaagib <45560956+AishaAlaagib@users.noreply.github.com>
2020-03-02 11:40:55 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -143,7 +143,7 @@ def images_to_probs(net, images):
     output = net(images)
     # convert output probabilities to predicted class
     _, preds_tensor = torch.max(output, 1)
-    preds = np.squeeze(preds_tensor.numpy())
+    preds = np.squeeze(preds_tensor.cpu())
     return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
 
 
