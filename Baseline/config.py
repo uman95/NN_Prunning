49ae80a68326706da875072b7c3e42d68725b80d
Aisha Alaagib <45560956+AishaAlaagib@users.noreply.github.com>
2020-04-10 01:07:32 +0000

    Update config.py

--- Baseline/config.py
+++ Baseline/config.py
@@ -12,8 +12,8 @@ parser.add_argument('--data', type=str, default='imagenet',
                     help='Dataset to be used')
 parser.add_argument('--batch_size', type=int, default=1024,
                     help='training batch size (default: 1024)')
-parser.add_argument('--test_batch_size', type=int, default=256,
-                    help='testing batch size (default: 256)')
+parser.add_argument('--test_batch_size', type=int, default=64,
+                    help='testing batch size (default: 64)')
 parser.add_argument('--epochs', type=int, default=150,
                     help='number of epochs to train for (Default: 150)')
 parser.add_argument('--lr', type=float, default=0.1,
