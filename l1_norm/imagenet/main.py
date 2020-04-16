b788c5d1783d253226a0bd192e322f4857144e38
Aisha Alaagib <45560956+AishaAlaagib@users.noreply.github.com>
2020-04-16 18:37:53 +0000

    Update main.py

--- l1_norm/imagenet/main.py
+++ l1_norm/imagenet/main.py
@@ -238,7 +238,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         # measure accuracy and record loss
         prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
-        losses.update(loss.data[0], input.item())
+        losses.update(loss.item(), input.item())
         top1.update(prec1[0], input.item())
         top5.update(prec5[0], input.item())
 
