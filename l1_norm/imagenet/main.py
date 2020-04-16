981c83ed7be486cdc7bba69d415b951daa78a920
Aisha Alaagib <45560956+AishaAlaagib@users.noreply.github.com>
2020-04-16 19:13:25 +0000

    Update main.py

--- l1_norm/imagenet/main.py
+++ l1_norm/imagenet/main.py
@@ -152,7 +152,7 @@ def main():
             
         train_loader = torch.utils.data.DataLoader(
                        train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
-                       num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+                       num_workers=1, pin_memory=True, sampler=train_sampler)
 
         val_loader   = torch.utils.data.DataLoader(
                        datasets.ImageFolder(valdir, transforms.Compose([
@@ -161,7 +161,7 @@ def main():
                        transforms.ToTensor(),
                        normalize, ])),
                        batch_size=args.batch_size, shuffle=False,
-                       num_workers=args.workers, pin_memory=True)
+                       num_workers=1, pin_memory=True)
         
     else: ## grey image
         
@@ -183,7 +183,7 @@ def main():
             
         train_loader = torch.utils.data.DataLoader(
                        train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
-                       num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+                       num_workers=1, pin_memory=True, sampler=train_sampler)
 
         val_loader   = torch.utils.data.DataLoader(
                        datasets.ImageFolder(valdir, transforms.Compose([
@@ -191,7 +191,7 @@ def main():
                        transforms.ToTensor(),
                        transforms.Normalize((0.5,), (0.5,)) ])),
                        batch_size=args.batch_size, shuffle=False,
-                       num_workers=args.workers, pin_memory=True)
+                       num_workers=1, pin_memory=True)
 
     ############ evaluate############################################
     if args.evaluate:
